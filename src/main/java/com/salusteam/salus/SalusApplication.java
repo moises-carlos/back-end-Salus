ec9221c5afc9acc3881cc68038e70bf5745833a7
Vitor Ferreira <vitorferr1412@gmail.com>
2026-05-05 00:40:22 -0300

    Commit inicial da estrutura do projeto

---
+++ src/main/java/com/salusteam/salus/SalusApplication.java
@@ -0,0 +1,13 @@
+package com.salusteam.salus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salusteam/salus/SalusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salusteam.salus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
